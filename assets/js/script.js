3e3a8879b89f9ed77864b02e60f4361845256e0d
Matthew Lister <mdlister24@hotmail.com>
2019-06-20 21:30:38 +0000

    Removed modal data that wasn't needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,39 @@
-// Code goes here
-
-global.$(document).ready(function() {
-  var map = null;
+/* global $ google */
+$(document).ready(function() {
+  var map;
   var myMarker;
   var myLatlng;
-
+  const mapID = document.getElementById("map1")
   function initializeGMap(lat, lng) {
-    myLatlng = new global.google.maps.LatLng(lat, lng);
+    myLatlng = new google.maps.LatLng(lat, lng);
 
     var myOptions = {
       zoom: 12,
       zoomControl: true,
       center: myLatlng,
-      mapTypeId: global.google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    map = new global.google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    map = new google.maps.Map(mapID, myOptions);
 
-    myMarker = new global.google.maps.Marker({
+    myMarker = new google.maps.Marker({
       position: myLatlng
     });
     myMarker.setMap(map);
   }
 
   // Re-init map before show modal
-  global.$('#myModal1').on('show.bs.modal', function(event) {
-    var button = global.$(event.relatedTarget);
+  $('#myModal1').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget);
     initializeGMap(button.data('lat'), button.data('lng'));
-    global.$("#location-map").css("width", "100%");
-    global.$("#map_canvas").css("width", "100%");
+    $("#location-map").css("width", "100%");
+    $("#map1").css("width", "100%");
   });
 
   // Trigger map resize event after modal shown
 
-  global.$('#myModal1').on('shown.bs.modal', function() {
-    global.google.maps.event.trigger(map, "resize");
+$('#myModal1').on('shown.bs.modal', function() {
+    google.maps.event.trigger(map, "resize");
     map.setCenter(myLatlng);
   });
 });
