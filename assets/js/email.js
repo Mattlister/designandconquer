da2769d5c64be4b412a7cca2a0f0656ad821e9c9
Matthew Lister <mdlister24@hotmail.com>
2019-07-04 20:10:43 +0000

    Added new copy to the modals

---
+++ assets/js/email.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="https://cdn.emailjs.com/sdk/2.3.2/email.min.js"></script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init("user_Mrr05wNVUIO4siIKtzb3l");
+   })();
+</script>
