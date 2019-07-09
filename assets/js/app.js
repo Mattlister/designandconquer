680f05f2fc4bdcab93789a7c9098bc10fac0adda
Matthew Lister <mdlister24@hotmail.com>
2019-07-09 22:03:27 +0000

    Relevant mapping code added for content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,56 @@
-const petsData = [
-  {
-    name: "Purrsloud",
-    species: "Cat",
-    favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
-    birthYear: 2017,
-    photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg"
-  },
-  {
-    name: "Barksalot",
-    species: "Dog",
-    birthYear: 2008,
-    photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg"
-  },
-  {
-    name: "Meowsalot",
-    species: "Cat",
-    favFoods: ["tuna", "catnip", "celery"],
-    birthYear: 2012,
-    photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg"
-  }
-];
+var map;
 
-document.getElementById("app").innerHTML = `<h1>Pets (${petsData.length} results)</h1>`
+function initialize() {
+  // Create a simple map.
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 16,
+    center: {
+      lat: 53.668398,
+      lng: -2.167713
+    },
+    styles: [{
+      "featureType": "landscape",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#ffffff"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#efefef"
+      }]
+    }, {
+      "featureType": "water",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "visibility": "on"
+      }, {
+        "color": "#dedddd"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "visibility": "on"
+      }, {
+        "color": "#efefef"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "labels.icon",
+      "stylers": [{
+        "visibility": "on"
+      }]
+    }]
+  });
+
+  // Load a GeoJSON from the same server as our demo.
+  //map.data.loadGeoJson('http://pixelsandcode.local:5757/map.json');
+}
+google.maps.event.addDomListener(window, 'load', initialize);
