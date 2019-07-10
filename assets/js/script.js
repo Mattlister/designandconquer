51ff5d6a2cd8d7a5eef25f75ecbf3cb70b22cf36
Matthew Lister <mdlister24@hotmail.com>
2019-07-10 12:04:21 +0000

    Adding new code for maps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,20 @@
-// Code goes here
+function initialize () {
+  let map = new google.maps.Map(document.getElementById('map_canvas'), {
+    center: new google.maps.LatLng(),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 18
+  })
+}
 
-global.$(document).ready(function() {
-  var map = null;
-  var myMarker;
-  var myLatlng;
-
-  function initializeGMap(lat, lng) {
-    myLatlng = new global.google.maps.LatLng(lat, lng);
-
-    var myOptions = {
-      zoom: 12,
-      zoomControl: true,
-      center: myLatlng,
-      mapTypeId: global.google.maps.MapTypeId.ROADMAP
-    };
-
-    map = new global.google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-    myMarker = new global.google.maps.Marker({
-      position: myLatlng
-    });
-    myMarker.setMap(map);
-  }
-
-  // Re-init map before show modal
-  global.$('#myModal1').on('show.bs.modal', function(event) {
-    var button = global.$(event.relatedTarget);
-    initializeGMap(button.data('lat'), button.data('lng'));
-    global.$("#location-map").css("width", "100%");
-    global.$("#map_canvas").css("width", "100%");
+for (let x in Queensland) {
+  let = location = Queensland[x];
+  let location = new google.maps.LatLng(location.lat,location.lng);
+  let marker = new google.maps.Marker({
+    position: location,
+    title: location.name,
+    map: map
   });
+ }
+}
+google.maps.event.addDOMListener(window, 'load', initialize);
 
-  // Trigger map resize event after modal shown
-
-  global.$('#myModal1').on('shown.bs.modal', function() {
-    global.google.maps.event.trigger(map, "resize");
-    map.setCenter(myLatlng);
-  });
-});
