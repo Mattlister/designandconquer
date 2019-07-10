92695eb52be5523f9aeaeb0ebfd6c94ec429835a
Matthew Lister <mdlister24@hotmail.com>
2019-07-10 15:57:37 +0000

    Updating files before leaving work for home use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 function initialize () {
   let map = new google.maps.Map(document.getElementById('map_canvas'), {
-    center: new google.maps.LatLng(),
+    center: new google.maps.LatLng(,),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 18
-  })
-}
+  });
+
 
 for (let x in Queensland) {
-  let = location = Queensland[x];
+  let location = Queensland[x];
   let location = new google.maps.LatLng(location.lat,location.lng);
   let marker = new google.maps.Marker({
     position: location,
