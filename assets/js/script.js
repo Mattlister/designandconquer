79fb7235c0ac4c2d3f968a414f0c6a7e3399e2d2
Matthew Lister <mdlister24@hotmail.com>
2019-07-11 07:39:05 +0000

    Working in connection with json file

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,15 @@ for (let x in Queensland) {
     title: location.name,
     map: map
   });
+  let infowindow = new google.maps.Infowindow({
+    content: location.name
+    });
+    
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker); 
+    });
+    
+    }
  }
 }
 google.maps.event.addDOMListener(window, 'load', initialize);
