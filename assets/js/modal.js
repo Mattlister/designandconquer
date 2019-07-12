f175098e6f3a912f7f26a1459de25f602222ba3f
Matthew Lister <mdlister24@hotmail.com>
2019-07-12 12:16:16 +0000

    Changes for map functionality

--- assets/js/modal.js
+++
@@ -1,8 +0,0 @@
-const name = 'John';
-
-const another = `Hello ${name},
-
-Thank you for contacting us in regards to booking an adventure.Array
-
-Regards,
-Matthew`;
--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ function initialize () {
 
 
 for (let x in locations) {
-  let location = Queensland[x];
+  let location = locations[x];
   let location1 = new google.maps.LatLng(location.lat,location.lng);
   let marker = new google.maps.Marker({
     position: location,
