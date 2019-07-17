45b952be7a3ac54ef74d92b4941d366bbda3ff57
Matthew Lister <mdlister24@hotmail.com>
2019-07-17 15:53:50 +0000

    Updating code in regards to mapping

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,8 @@
-fetch('assets/js/locations.json')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    console.log(response);
-  });
-  
-  fetch('assets/js/locations.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    myFunction(response);
-  });
-  
-  function myFunction(response) {
-	/* do something with response variable */
-}
-
-$(document).ready(function() {
-  var map;
+var map;
   var myMarker;
   var myLatlng;
   var mapID = document.getElementById("map1");
+
   function initializeGMap(lat, lng) {
     myLatlng = new google.maps.LatLng(lat, lng);
 
@@ -40,31 +20,3 @@ $(document).ready(function() {
     });
     myMarker.setMap(map);
     }
-    
-      // Re-init map before show modal
-  $('#myModal1').on('show.bs.modal', function(event) {
-    let button = $(event.relatedTarget);
-    initializeGMap(button.data('lat'), button.data('lng'));
-    $("#location-map").css("width", "100%");
-    $("#map1").css("width", "100%");
-  });
-
-  
-  for (let x in locations.json) {
-}
-  let location = locations.json[x];
-  let location1 = new google.maps.LatLng(location.lat,location.lng);
-  let marker = new google.maps.Marker({
-    position: location,
-    title: location.name,
-    map: map
-  });
-
-
-  // Trigger map resize event after modal shown
-
-$('#myModal1').on('shown.bs.modal', function() {
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(myLatlng);
-  });
-});
