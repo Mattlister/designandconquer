353f4d92f1555730444a85f53d627f89b5d8fa4c
Matthew Lister <mdlister24@hotmail.com>
2019-08-15 21:41:39 +0000

    New map for markers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,29 @@
- var map;
-  var mapID = document.getElementById("map1");
-  function initMap(lat, lng) {
+let map = new google.maps.Map(document.getElementById('#map1'), {
+  zoom: 10,
+  center: new google.maps.LatLng(lat, lng),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
 
-    var myLatlng = new google.maps.LatLng(lat, lng);
-    var myOptions = {
-      zoom: 12,
-      zoomControl: true,
-      center: myLatlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-      
-    };
-
-var marker, i;
-
-    for(var i = 0; i < multiMarkers.length; i++) { 
-        var coords = results.features[i].geometry.coordinates;
-        marker = new google.maps.Marker({
-        position: new google.maps.myLatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
-         
-        
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                myLatlng.setContent(markers[i][0]);
-                myLatlng.open(map, marker);
-            };
-        })(marker, i));
-    }
-
-
-    var map = new google.maps.Map(mapID, myOptions);
-    
-    
-
-    myMarker2 = new google.maps.Marker({
-      position: myLatlng,
-    });
-    myMarker2.setMap(map);
-    }
-     fetch('assets/js/locations.json')
+fetch('assets/data/locations.json')
   .then(res => res.json())
   .then(function(data) {
       console.log(data);
-      let loc = getLatLng(data);
-      data.forEach(adven => {
-        console.log(adven);
-        multiMarkers = {
-          lat: adven.lat,
-          lng: adven.lng
-        };
-        myMarker2 = new google.maps.Marker({
-            position: multiMarkers,
-            map: map,
-            
-        });    
+    for (let i = 0; i < data.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.lng),
+        map: map
       });
-  })
-  .catch(function(error) {
-    console.log(error);
+    }
   });
   
-$('#myModal1').on('show.bs.modal', function(event) {
+  
+  $('#myModal1').on('show.bs.modal', function(event) {
     let relatedTarget = $(event.relatedTarget);
-    initMap(parseFloat(relatedTarget.data('lat')), parseFloat(relatedTarget.data('lng')));
-    $("#location-map").css("width", "100%");
+    $("#locations-map1").css("width", "100%");
     $("#map1").css("width", "100%");
   });
   
-function getLatLng(jsonElement) {
+  function getLatLng(jsonElement) {
   let locations = [];
   jsonElement.forEach(element => {
       
@@ -84,18 +39,17 @@ function getLatLng(jsonElement) {
   
   return locations;
 }
-
-
-$(function () {
+  
+  
+  $(function () {
         $("#btnClosePopup").click(function () {
             $("#modal-content").modal("hide");
         });
     });
-
         function disappearModalTwo() {
             document.getElementById("exampleModal_One").style.display='none';
         }
-
 function myFunction() {
   alert("Thanks for your email, we'll be in touch real soon!!!");
 }
+ 
