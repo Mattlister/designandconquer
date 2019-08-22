5f5634eb5d760edf052be44e1e84453eefb72697
Matthew Lister <mdlister24@hotmail.com>
2019-08-22 11:32:11 +0000

    Moved data inside the  ready function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,48 @@ $(document).ready(function() {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     
-
+fetch('assets/js/locations.json')
+  .then(res => res.json())
+  .then(function(data) {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+      return myMarker = new google.maps.LatLng({
+        position: (data.lat, data.lng),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+     });
+      
+      initMap(parseFloat(button.data('lat')), parseFloat(button.data('lng')))
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat[i][1], lat[i][2]),
+        map:map,
+        title: myLatlng[i][0]
+      });
+      data.forEach(element => {
+        element.forEach(data => {
+          myMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lng),
+            map:map,
+          });
+        });
+      });
+      {
+          var markerData = JSON.parse(json);
+          
+      }
+      
+      
+    var myLatlng = new google.maps.LatLng(lat, lng);
+    var myLatlng = new google.maps.Marker({
+        position: (myLatlng),
+        map:map,
+    });
+      console.log(myMarker);
+      myMarker.setMap(mapID);
+    }
+    
+  });
+  
+ 
  
     map = new google.maps.Map(mapID, myOptions);
     let myMarker = new google.maps.Marker({
