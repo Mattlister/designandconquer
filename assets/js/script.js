e1aed9f7a245938f91cc9dae5cd5ae02b8591ad3
Matthew Lister <mdlister24@hotmail.com>
2019-08-23 15:27:31 +0000

    Cross referencing 2 maps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-/* global $ google */
 $(document).ready(function() {
 var map = document.getElementById("map1");
 var myLatlng; 
@@ -28,4 +27,34 @@ $("#map1").css("width", "100%");
 
 });
 
+});
+
+$(document).ready(function() {
+  var mapID = document.getElementById('map');
+  function initMap(lat, lng) {
+    var myOptions = {
+      zoom: 12,
+      center:{lat:-37.75,lng: 145.01667}
+    };
+    
+    let map = new google.maps.Map(mapID, myOptions);
+    
+
+    var transitLayer = new google.maps.TransitLayer();
+transitLayer.setMap(map);
+    }
+    
+   $('#myModal1').on('show.bs.modal', function(event) {
+    let button = $(event.relatedTarget);
+    initMap(button.data('lat'), button.data('lng'));
+    $("#location-map").css("width", "100%");
+    $("#map").css("width", "100%");
+  });
+  
+   //New Map
+   
+   
+    
+  
+    
 });
