c6fc92af4a0182a2c738b65652ed96d690ddbb12
Matthew Lister <mdlister24@hotmail.com>
2019-08-23 09:26:26 +0000

    Working backwards to resolve map markers issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,39 @@
+/* global $ google */
 $(document).ready(function() {
-  var mapID = document.getElementById('map');
-  function initMap(lat, lng) {
+  var map;
+  var myMarker;
+  var myLatlng;
+  const mapID = document.getElementById("map1")
+  function initializeGMap(lat, lng) {
+    myLatlng = new google.maps.LatLng(lat, lng);
+
     var myOptions = {
       zoom: 12,
-      center:{lat:-37.75,lng: 145.01667}
+      zoomControl: true,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    
-    let map = new google.maps.Map(mapID, myOptions);
-    
 
-    var transitLayer = new google.maps.TransitLayer();
-transitLayer.setMap(map);
-    }
-    
-   $('#myModal1').on('show.bs.modal', function(event) {
-    let button = $(event.relatedTarget);
-    initMap(button.data('lat'), button.data('lng'));
+    map = new google.maps.Map(mapID, myOptions);
+
+    myMarker = new google.maps.Marker({
+      position: myLatlng
+    });
+    myMarker.setMap(map);
+  }
+
+  // Re-init map before show modal
+  $('#myModal1').on('show.bs.modal', function(event) {
+    var button = $(event.relatedTarget);
+    initializeGMap(button.data('lat'), button.data('lng'));
     $("#location-map").css("width", "100%");
-    $("#map").css("width", "100%");
+    $("#map1").css("width", "100%");
   });
-  
-   //New Map
-    
-  
-    
+
+  // Trigger map resize event after modal shown
+
+$('#myModal1').on('shown.bs.modal', function() {
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(myLatlng);
+  });
 });
