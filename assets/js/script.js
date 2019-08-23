3981c9ce643296ceef2c3caa4bfac59d525c2816
Matthew Lister <mdlister24@hotmail.com>
2019-08-23 13:32:26 +0000

    Using older code to produce markers in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,31 @@
 /* global $ google */
 $(document).ready(function() {
-  var map;
-  var myMarker;
-  var myLatlng;
-  const mapID = document.getElementById("map1")
-  function initializeGMap(lat, lng) {
-    myLatlng = new google.maps.LatLng(lat, lng);
+var map = document.getElementById("map1");
+var myLatlng; 
+const mapID = document.getElementById("map1");
+function initializeGMap(lat, lng) {
+myLatlng = new google.maps.LatLng(lat, lng);
+var myOptions = {
+zoom: 12,
+zoomControl: true,
+center: myLatlng,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+map = new google.maps.Map(mapID, myOptions);
+myMarker = new google.maps.Marker({
+position: myLatlng
+});
+myMarker.setMap(map);
+}
 
-    var myOptions = {
-      zoom: 12,
-      zoomControl: true,
-      center: myLatlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
 
-    map = new google.maps.Map(mapID, myOptions);
+// Re-init map before show modal
+$('#myModal1').on('show.bs.modal', function(event) {
+var button = $(event.relatedTarget);
+initializeGMap(button.data('lat'), button.data('lng'));
+$("#location-map").css("width", "100%");
+$("#map1").css("width", "100%");
 
-    myMarker = new google.maps.Marker({
-      position: myLatlng
-    });
-    myMarker.setMap(map);
-  }
+});
 
-  // Re-init map before show modal
-  $('#myModal1').on('show.bs.modal', function(event) {
-    var button = $(event.relatedTarget);
-    initializeGMap(button.data('lat'), button.data('lng'));
-    $("#location-map").css("width", "100%");
-    $("#map1").css("width", "100%");
-  });
-
-  // Trigger map resize event after modal shown
-
-$('#myModal1').on('shown.bs.modal', function() {
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(myLatlng);
-  });
 });
