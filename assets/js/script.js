e868878e174b6d77295f6cf25e9b5d7637a731ce
Matthew Lister <mdlister24@hotmail.com>
2019-08-24 14:53:07 +0000

    Issues pushing new content to github

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,47 @@
 $(document).ready(function() {
-  var mapID = document.getElementById('map1');
-  function initMap(lat, lng) {
-    var myOptions = {
+let map;
+let myMarker;
+let myLatLng;
+  var mapID = document.getElementById("map1");
+  function initializeGMap(lat, lng) {
+    myLatlng = new google.maps.LatLng(lat, lng);
+new
+ var myOptions = {
       zoom: 12,
-     center:{lat:43.3601,lng: -71.0589},
+      zoomControl: true,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     
-    let map = new google.maps.Map(mapID, myOptions);
-    
-    
-    var marker = new google.maps.Marker({
-        position:{lat:42.4668,lng:-70.9495},
-    })
-    
-    let marker = new google.maps.Marker({
-        
-    })
-    
-    
-    var transitLayer = new google.maps.TransitLayer();
-transitLayer.setMap(map);
+
+    map = new google.maps.Map(mapID, myOptions);
+
+    myMarker = new google.maps.Marker({
+      position: myLatlng
+      
+    });
+    fetch('assets/js/locations.json')
+  .then(res => res.json())
+  .then(function(data) {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+      return myMarker = new google.maps.LatLng({
+        position: (data.lat, data.lng),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      console.log(myMarker);
+      myMarker.setMap(mapID);
     }
     
-   $('#myModal1').on('show.bs.modal', function(event) {
-    let button = $(event.relatedTarget);
-    initMap(button.data('lat'), button.data('lng'));
-    $("#location-map").css("width", "100%");
-    $("#map").css("width", "100%");
   });
-  
-   //New Map
-   
-   
-    
-  
-    
-});
-
-$(document).ready(function() {
-const mapID = document.getElementById("map1");
-function initializeGMap(lat, lng) {
-myLatlng = new google.maps.LatLng(lat, lng);
-var myOptions = {
-zoom: 12,
-zoomControl: true,
- center:{lat:43.3601,lng: -71.0589},
-mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-map = new google.maps.Map(mapID, myOptions);
-myMarker = new google.maps.Marker({
-position: myLatlng
-});
-myMarker.setMap(map);
-}
-
-
-// Re-init map before show modal
-$('#myModal1').on('show.bs.modal', function(event) {
-var button = $(event.relatedTarget);
-initializeGMap(button.data('lat'), button.data('lng'));
-$("#location-map").css("width", "100%");
-$("#map1").css("width", "100%");
-
-});
-
-});
-
-$(document).ready(function() {
-  var mapID = document.getElementById('map1');
-  function initMap(lat, lng) {
-    var myOptions = {
-      zoom: 12,
-     center:{lat:43.3601,lng: -71.0589},
-    };
-    
-    let map = new google.maps.Map(mapID, myOptions);
-    
-
-    var transitLayer = new google.maps.TransitLayer();
-transitLayer.setMap(map);
+    myMarker.setMap(map);
     }
     
-   $('#myModal1').on('show.bs.modal', function(event) {
+     $('#myModal1').on('show.bs.modal', function(event) {
     let button = $(event.relatedTarget);
-    initMap(button.data('lat'), button.data('lng'));
+    initializeGMap(button.data('lat'), button.data('lng'));
     $("#location-map").css("width", "100%");
-    $("#map").css("width", "100%");
+    $("#map1").css("width", "100%");
   });
-  
-   //New Map
-   
-   
-    
-  
-    
 });
+
