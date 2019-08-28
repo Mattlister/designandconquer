588f121637f9cb76bdd5e4384de9aaa4208dd4b1
Matthew Lister <mdlister24@hotmail.com>
2019-08-28 14:29:32 +0000

    Reset back to old code after mistakes made

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,71 +10,61 @@ function initializeGMap(lat, lng) {
   
 myLatlng = new google.maps.LatLng(lat, lng);
 
-let bike = [{name: "Google Location 1", lat:-37.623900, lng: 144.707700},
+let bike = [{name: "Google Location 1", lat:-37.623900, lng: 144.707700 },
 {name: "Google Location 2", lat: -26.785780, lng: 153.110300},
-{name: "Google Location 3", lat: -37.796270, lng: 146.974500}];
-
-
+{name: "Google Location 3", lat: -37.796270, lng: 146.974500}]
 
 //Yosamite coords data - done
 
 let climb = [{name: "Google Location 1", lat:37.865101, lng:-119.538330},
 {name: "Google Location 2", lat: 37.746036, lng: -119.53294},
-{name: "Google Location 3", lat: 38.8015, lng: -120.133}];
-
-$(document).ready(function(){
-  $('#Yosamiteclimbingmodal').on('shown.bs.modal', function(){
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(new google.maps.LatLng(37.865101, -119.538330));
-  });
-});
-
+{name: "Google Location 3", lat: 38.8015, lng: -120.133}]
 
 //Everest coords data - done
 
 let everest = [{name: "Google Location 1", lat:27.986065, lng:86.922623},
 {name: "Google Location 2", lat: 28.004333316, lng: 86.855663244},
-{name: "Google Location 3", lat:  27.816667, lng: 86.716667}];
+{name: "Google Location 3", lat:  27.816667, lng: 86.716667}]
 
 
 //Banzai coords data - done
 
 let banzai = [{name: "Google Location 1", lat:21.664019, lng:-158.053852},
 {name: "Google Location 2", lat: 21.640296, lng: -158.063779},
-{name: "Google Location 3", lat: 21.659830694, lng: -158.053166454}];
+{name: "Google Location 3", lat: 21.659830694, lng: -158.053166454}]
 
 
 //Alps coords data - done
 
 let alps = [{name: "Google Location 1", lat:45.45916483, lng:6.90249639},
 {name: "Google Location 2", lat: 45.292165498, lng: 6.574664368},
-{name: "Google Location 3", lat: 45.004333316, lng: 6.119666188}];
+{name: "Google Location 3", lat: 45.004333316, lng: 6.119666188}]
 
 
 //Vicfalls skydive coords data
 
 let vicfalls = [{name: "Google Location 1", lat:-17.924444, lng:25.856667},
 {name: "Google Location 2", lat: 17.9318, lng: 25.8256},
-{name: "Google Location 3", lat: 17.9049, lng: 25.9379}];
+{name: "Google Location 3", lat: 17.9049, lng: 25.9379}]
 
 
 //Australian coords data - done
 
-let neptune = [{name: "Google Location 1", lat:34.7240, lng:135.8611}];
+let neptune = [{name: "Google Location 1", lat:34.7240, lng:135.8611}]
 
 
 //River nile coords data - done
 
 let nile = [{name: "Google Location 1", lat:29.533438, lng:31.270695},
 {name: "Google Location 2", lat: 21.701324, lng: 31.176766},
-{name: "Google Location 3", lat: 16.783861, lng: 32.898682}];
+{name: "Google Location 3", lat: 16.783861, lng: 32.898682}]
 
 
 //Dubai Buggying coords data - done
 
 let dubai = [{name: "Google Location 1", lat:25.1840, lng:55.2596},
 {name: "Google Location 2", lat: 25.1872, lng: 55.2812},
-{name: "Google Location 3", lat: 25.2768, lng: 55.3407}];
+{name: "Google Location 3", lat: 25.2768, lng: 55.3407}]
 
 
 
@@ -161,7 +151,11 @@ for (let x = 0; x < dubai.length; x++) {
 
 
 
-let VT = new google.maps.LatLng(-37.623900, 144.707700);
+
+// lat: 37.83333, lng: -119.49998
+
+let VT = new google.maps.LatLng(-26.785780, 153.110300);
+
 
 var myOptions = {
     zoom: 12,
@@ -169,13 +163,6 @@ var myOptions = {
     center: VT
 };
 
-let ST = new google.maps.LatLng(37.865101, -119.538330);
-
-var myOptions1 = {
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: ST
-};
 
 
 map = new google.maps.Map(document.getElementById("map_go"), myOptions);
@@ -183,10 +170,6 @@ map = new google.maps.Map(document.getElementById("map_go"), myOptions);
 // when code ran
 addmarker();
 
-map = new google.maps.Map(document.getElementById("map_go1"), myOptions1);
-// This add marker can probably be removed i just added it to show a marker
-// when code ran
-addmarker();
 
 
 
@@ -223,6 +206,7 @@ for (let x = 0; x < marks.length; x++) {
 });
 
 
+    
 }
     
   $('#myModal1').on('show.bs.modal', function(event) {
@@ -232,18 +216,5 @@ for (let x = 0; x < marks.length; x++) {
       $("#map_go").css("width", "100%");
   });
   
-   function addMarker(location) {
-        marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
-    }
-
-    // Testing the addMarker function
-    CentralPark = new google.maps.LatLng(37.7699298, -122.4469157);
-    addMarker(CentralPark);
-    
-  
   
 });
-
