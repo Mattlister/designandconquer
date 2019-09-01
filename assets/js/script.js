9cb8becca32f132bb49943b9fd2a319a028318e6
Matthew Lister <mdlister24@hotmail.com>
2019-09-01 10:18:02 +0000

    Back to the drawing board as I don't understand overwriting

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,63 +8,66 @@ var mapID = document.getElementById("map_go");
 function initializeGMap(lat, lng) {
   
   
-
+myLatlng = new google.maps.LatLng(lat, lng);
 
 let bike = [{name: "Google Location 1", lat:-37.623900, lng: 144.707700 },
 {name: "Google Location 2", lat: -26.785780, lng: 153.110300},
-{name: "Google Location 3", lat: -37.796270, lng: 146.974500}];
+{name: "Google Location 3", lat: -37.796270, lng: 146.974500}]
 
 //Yosamite coords data - done
 
 let climb = [{name: "Google Location 1", lat:37.865101, lng:-119.538330},
 {name: "Google Location 2", lat: 37.746036, lng: -119.53294},
-{name: "Google Location 3", lat: 38.8015, lng: -120.133}];
+{name: "Google Location 3", lat: 38.8015, lng: -120.133}]
 
 //Everest coords data - done
 
 let everest = [{name: "Google Location 1", lat:27.986065, lng:86.922623},
 {name: "Google Location 2", lat: 28.004333316, lng: 86.855663244},
-{name: "Google Location 3", lat:  27.816667, lng: 86.716667}];
+{name: "Google Location 3", lat:  27.816667, lng: 86.716667}]
 
 
 //Banzai coords data - done
 
 let banzai = [{name: "Google Location 1", lat:21.664019, lng:-158.053852},
 {name: "Google Location 2", lat: 21.640296, lng: -158.063779},
-{name: "Google Location 3", lat: 21.659830694, lng: -158.053166454}];
+{name: "Google Location 3", lat: 21.659830694, lng: -158.053166454}]
 
 
 //Alps coords data - done
 
 let alps = [{name: "Google Location 1", lat:45.45916483, lng:6.90249639},
 {name: "Google Location 2", lat: 45.292165498, lng: 6.574664368},
-{name: "Google Location 3", lat: 45.004333316, lng: 6.119666188}];
+{name: "Google Location 3", lat: 45.004333316, lng: 6.119666188}]
 
 
 //Vicfalls skydive coords data
 
 let vicfalls = [{name: "Google Location 1", lat:-17.924444, lng:25.856667},
 {name: "Google Location 2", lat: 17.9318, lng: 25.8256},
-{name: "Google Location 3", lat: 17.9049, lng: 25.9379}];
+{name: "Google Location 3", lat: 17.9049, lng: 25.9379}]
 
 
 //Australian coords data - done
 
-let neptune = [{name: "Google Location 1", lat:34.7240, lng:135.8611}];
+let neptune = [{name: "Google Location 1", lat:34.7240, lng:135.8611}]
 
 
 //River nile coords data - done
 
 let nile = [{name: "Google Location 1", lat:29.533438, lng:31.270695},
 {name: "Google Location 2", lat: 21.701324, lng: 31.176766},
-{name: "Google Location 3", lat: 16.783861, lng: 32.898682}];
+{name: "Google Location 3", lat: 16.783861, lng: 32.898682}]
 
 
 //Dubai Buggying coords data - done
 
 let dubai = [{name: "Google Location 1", lat:25.1840, lng:55.2596},
 {name: "Google Location 2", lat: 25.1872, lng: 55.2812},
-{name: "Google Location 3", lat: 25.2768, lng: 55.3407}];
+{name: "Google Location 3", lat: 25.2768, lng: 55.3407}]
+
+
+
 
 
 //map marker details
@@ -146,8 +149,12 @@ for (let x = 0; x < dubai.length; x++) {
 }
 
 
+
+
+
+// lat: 37.83333, lng: -119.49998
+
 let VT = new google.maps.LatLng(-26.785780, 153.110300);
-let mapID = document.getElementById('map_go');
 
 
 var myOptions = {
@@ -157,44 +164,14 @@ var myOptions = {
 };
 
 
+
 map = new google.maps.Map(document.getElementById("map_go"), myOptions);
 // This add marker can probably be removed i just added it to show a marker
 // when code ran
 addmarker();
 
-//
-
-let ST = new google.maps.LatLng(37.865101, -119.538330);
-let map1ID = document.getElementById('map_canvas');
 
 
-
-var myOptions1 = {
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: ST
-};
-
-map = new google.maps.Map(document.getElementById('map-canvas'), myOptions1);
-addmarker();
-
-//
-
-let ET = new google.maps.LatLng(27.986065, 86.922623);
-let map2ID = document.getElementById('map_test');
-
-
-
-var myOptions2 = {
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: ET
-};
-
-map = new google.maps.Map(document.getElementById('map-test'), myOptions2);
-addmarker();
-
-//
 
 function addmarker(latilongi) {
     var marker = new google.maps.Marker({
@@ -203,8 +180,9 @@ function addmarker(latilongi) {
         draggable: true,
         map: map
     });
-
-
+    
+    
+    
     
     // Line below centers map on marker I assume this wont be needed as your
     // adding multiple markers but left it just in case
