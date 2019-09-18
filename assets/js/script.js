8aa277d76a7811371ba89f50644109b7a5036537
Matthew Lister <mdlister24@hotmail.com>
2019-09-18 15:47:27 +0000

    New information added to start working on dynamic settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,44 +8,44 @@ let myLatLng;
             //Queensland
 
             const bike = [{name: "Google Location 1", lat:-37.623900, lng: 144.707700},
-            {name: "Google Location 2", lat: -26.785780, lng: 153.110300},
-            {name: "Google Location 3", lat: -37.796270, lng: 146.974500}];
+                          {name: "Google Location 2", lat: -26.785780, lng: 153.110300},
+                          {name: "Google Location 3", lat: -37.796270, lng: 146.974500}];
             
             //Yosamite
             
             const climb = [{name: "Google Location 1", lat:37.865101, lng:-119.538330},
-            {name: "Google Location 2", lat: 37.746036, lng: -119.53294},
-            {name: "Google Location 3", lat: 38.8015, lng: -120.133}];
-            
+                           {name: "Google Location 2", lat: 37.746036, lng: -119.53294},
+                           {name: "Google Location 3", lat: 38.8015, lng: -120.133}];
+                            
             //Everest
             
             const climbe = [{name: "Google Location 1", lat:27.986065, lng:86.922623},
-            {name: "Google Location 2", lat: 28.004333316, lng: 86.855663244},
-            {name: "Google Location 3", lat:  27.816667, lng: 86.716667}];
+                            {name: "Google Location 2", lat: 28.004333316, lng: 86.855663244},
+                            {name: "Google Location 3", lat:  27.816667, lng: 86.716667}];
             
             //Banzai
             
             const surf = [{name: "Google Location 1", lat:21.664019, lng:-158.053852},
-            {name: "Google Location 2", lat: 21.640296, lng: -158.063779},
-            {name: "Google Location 3", lat: 21.659830694, lng: -158.053166454}];
+                           {name: "Google Location 2", lat: 21.640296, lng: -158.063779},
+                           {name: "Google Location 3", lat: 21.659830694, lng: -158.053166454}];
             
             const ski = [{name: "Google Location 1", lat:45.45916483, lng:6.90249639},
-            {name: "Google Location 2", lat: 45.292165498, lng: 6.574664368},
-            {name: "Google Location 3", lat: 45.004333316, lng: 6.119666188}];
+                          {name: "Google Location 2", lat: 45.292165498, lng: 6.574664368},
+                          {name: "Google Location 3", lat: 45.004333316, lng: 6.119666188}];
             
             const jump = [{name: "Google Location 1", lat:-17.924444, lng:25.856667},
-            {name: "Google Location 2", lat: 17.9318, lng: 25.8256},
-            {name: "Google Location 3", lat: 17.9049, lng: 25.9379}];
+                          {name: "Google Location 2", lat: 17.9318, lng: 25.8256},
+                          {name: "Google Location 3", lat: 17.9049, lng: 25.9379}];
             
             const dive = [{name: "Google Location 1", lat:-35.336240, lng:136.118428}];
             
             const cruise = [{name: "Google Location 1", lat:29.533438, lng:31.270695},
-            {name: "Google Location 2", lat: 21.701324, lng: 31.176766},
-            {name: "Google Location 3", lat: 16.783861, lng: 32.898682}];
+                            {name: "Google Location 2", lat: 21.701324, lng: 31.176766},
+                            {name: "Google Location 3", lat: 16.783861, lng: 32.898682}];
             
             const buggy = [{name: "Google Location 1", lat:25.1840, lng:55.2596},
-            {name: "Google Location 2", lat: 25.1872, lng: 55.2812},
-            {name: "Google Location 3", lat: 25.2768, lng: 55.3407}];
+                            {name: "Google Location 2", lat: 25.1872, lng: 55.2812},
+                            {name: "Google Location 3", lat: 25.2768, lng: 55.3407}];
 
 
 function initializeGMap(lat, lng) {
@@ -84,8 +84,7 @@ function addmarker(latilongi) {
         }
         var marker = new google.maps.Marker({
         position: new google.maps.LatLng(bike[i].lat, bike[i].lng),
-        
-        title: 'Queensland has some great mountain biking trails. Check out our 3 locations to find some unbelievable biking experiences',
+                title: 'Queensland has some great mountain biking trails. Check out our 3 locations to find some unbelievable biking experiences',
         draggable: true,
         map: map0,
         icon: image
@@ -93,10 +92,12 @@ function addmarker(latilongi) {
     }
 }
 
+
 // add marker settings
 $('#btnaddmarker1').on('click', function() {
     addmarker1(climb[0].lat, climb[0].lng);
 });
+
 
 
 function addmarker1(latilongi) {
