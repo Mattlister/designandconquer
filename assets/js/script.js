403193513c1bb2fb42dca01a297c944f6ea4df06
Matthew Lister <mdlister24@hotmail.com>
2019-09-23 08:52:49 +0000

    New dynamic code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,73 +3,76 @@ $(document).ready(function() {
 //let myMarker;
 //let myLatLng;
 
-//Lat and Long
 
-            //Queensland
+// New data
 
-          //  const bike = [{name: "Google Location 1", lat:-37.623900, lng: 144.707700},
-          //    {name: "Google Location 2", lat: -26.785780, lng: 153.110300},
-          //  {name: "Google Location 3", lat: -37.796270, lng: 146.974500}];
-            
-            //Yosamite
-            
-            const climb = [{name: "Google Location 1", lat:37.865101, lng:-119.538330},
-            {name: "Google Location 2", lat: 37.746036, lng: -119.53294},
-            {name: "Google Location 3", lat: 38.8015, lng: -120.133}];
-            
-            //Everest
-            
-            const climbe = [{name: "Google Location 1", lat:27.986065, lng:86.922623},
-            {name: "Google Location 2", lat: 28.004333316, lng: 86.855663244},
-            {name: "Google Location 3", lat:  27.816667, lng: 86.716667}];
-            
-            //Banzai
-            
-            const surf = [{name: "Google Location 1", lat:21.664019, lng:-158.053852},
-            {name: "Google Location 2", lat: 21.640296, lng: -158.063779},
-            {name: "Google Location 3", lat: 21.659830694, lng: -158.053166454}];
-            
-            const ski = [{name: "Google Location 1", lat:45.45916483, lng:6.90249639},
-            {name: "Google Location 2", lat: 45.292165498, lng: 6.574664368},
-            {name: "Google Location 3", lat: 45.004333316, lng: 6.119666188}];
-            
-            const jump = [{name: "Google Location 1", lat:-17.924444, lng:25.856667},
-            {name: "Google Location 2", lat: 17.9318, lng: 25.8256},
-            {name: "Google Location 3", lat: 17.9049, lng: 25.9379}];
-            
-            const dive = [{name: "Google Location 1", lat:-35.336240, lng:136.118428}];
-            
-            const cruise = [{name: "Google Location 1", lat:29.533438, lng:31.270695},
-            {name: "Google Location 2", lat: 21.701324, lng: 31.176766},
-            {name: "Google Location 3", lat: 16.783861, lng: 32.898682}];
-            
-            const buggy = [{name: "Google Location 1", lat:25.1840, lng:55.2596},
-            {name: "Google Location 2", lat: 25.1872, lng: 55.2812},
-            {name: "Google Location 3", lat: 25.2768, lng: 55.3407}];
+const bike = [{ name: "Google Location 1", lat: -37.623900, lng: 144.707700 },
+    { name: "Google Location 2", lat: -26.785780, lng: 153.110300 },
+    { name: "Google Location 3", lat: -37.796270, lng: 146.974500 }];
 
-const destinations = [
-    {
-       name: 'Google Location 1',
-       lat: -37.623900, 
-       lng: 144.707700,
-       icon: 'assets/images/icons8-cycling-mountain-bike-50.png'
-     },
-     
-      {
-       name: 'Google Location 2',
-       lat: -26.785780,
-       lng: 153.110300,
-       icon: 'assets/images/icons8-cycling-mountain-bike-50.png'
-     },
-      {
-       name: 'Google Location 3',
-       lat: -37.796270,
-       lng: 146.974500,
-       icon: 'assets/images/icons8-cycling-mountain-bike-50.png'
-     },
-    
-    ];
-    
+
+
+const climb = [{ name: "Google Location 1", lat: 37.865101, lng: -119.538330 },
+    { name: "Google Location 2", lat: 37.746036, lng: -119.53294 },
+    { name: "Google Location 3", lat: 38.8015, lng: -120.133 }];
+
+const climbe = [{ name: "Google Location 1", lat: 27.986065, lng: 86.922623 },
+    { name: "Google Location 2", lat: 28.004333316, lng: 86.855663244 },
+    { name: "Google Location 3", lat: 27.816667, lng: 86.716667 }];
+
+const surf = [{ name: "Google Location 1", lat: 21.664019, lng: -158.053852 },
+    { name: "Google Location 2", lat: 21.640296, lng: -158.063779 },
+    { name: "Google Location 3", lat: 21.659830694, lng: -158.053166454 }];
+
+const ski = [{ name: "Google Location 1", lat: 45.45916483, lng: 6.90249639 },
+    { name: "Google Location 2", lat: 45.292165498, lng: 6.574664368 },
+    { name: "Google Location 3", lat: 45.004333316, lng: 6.119666188 }];
+
+const jump = [{ name: "Google Location 1", lat: -17.924444, lng: 25.856667 },
+    { name: "Google Location 2", lat: 17.9318, lng: 25.8256 },
+    { name: "Google Location 3", lat: 17.9049, lng: 25.9379 }];
+            
+const dive = [{ name: "Google Location 1", lat: -35.336240, lng: 136.118428 }];
+
+const cruise = [{ name: "Google Location 1", lat: 29.533438, lng: 31.270695 },
+    { name: "Google Location 2", lat: 21.701324, lng: 31.176766 },
+    { name: "Google Location 3", lat: 16.783861, lng: 32.898682 }];
+
+const buggy = [{ name: "Google Location 1", lat: 25.1840, lng: 55.2596 },
+    { name: "Google Location 2", lat: 25.1872, lng: 55.2812 },
+    { name: "Google Location 3", lat: 25.2768, lng: 55.3407 }];
+            
+               
+            
+function addMarker(destinations) {
+       console.log('Return' + destinations);
+    for (var i = 0; i < destinations.length; i++) {
+      var marker = new google.maps.Marker({
+        position: (destinations [i].lat, destinations [i].lng),
+        title: destinations [i].name,
+        draggable: true,
+        map: 'map' + i,
+        icon: destinations [i].icon
+      });
+    }
+  }
+  
+addMarker(bike);
+addMarker(climb);
+addMarker(climbe);
+addMarker(surf);
+addMarker(ski);
+addMarker(jump);
+addMarker(dive);
+addMarker(cruise);
+addMarker(buggy);
+
+
+
+
+
+
+
 $('.buttons').click(function(e) {
     e.preventDefault();
     let id = this.id
@@ -86,28 +89,7 @@ $('.buttons').click(function(e) {
 });
 
 
-  function addMarker(destinations) {
-    for (var i = 0; i < activity.length; i++) {
-      var marker = new google.maps.Marker({
-        position: (destinations[i].lat, destinations[i].lng),
-        title: activity[i].name,
-        draggable: true,
-        map: 'map' + i,
-        icon: activity[i].icon
-      });
-    }
-  }
- 
- 
-addMarker('bike');
-addMarker('climb');
-addMarker('cimbe');
-addMarker('surf');
-addMarker('ski');
-addMarker('jump');
-addMarker('dive');
-addMarker('cruise');
-addMarker('buggy')
+  
 
   
   
