407d34e3bfb972a39c52eacc1edc642f561c8003
Matthew Lister <mdlister24@hotmail.com>
2019-09-27 14:11:47 +0000

    Map markers finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,9 +50,9 @@ function mapMarker(map) {
  
 
 // Queensland
-    const bike = [{ name: "Google Location 1", lat: -37.623900, lng: 144.707700 },
-    { name: "Google Location 2", lat: -26.785780, lng: 153.110300 },
-    { name: "Google Location 3", lat: -37.796270, lng: 146.974500 },
+    const bike = [{ name: "A great bike riding location", lat: -37.623900, lng: 144.707700 },
+    { name: "The downhills from this ride are amazing", lat: -26.785780, lng: 153.110300 },
+    { name: "Here you'll find some of the highest jumps in the country", lat: -37.796270, lng: 146.974500 },
     { map: map0 },
     { title: 'Queensland has some great mountain biking trails. Check out our 3 locations to find some unbelievable biking experiences'},
     { icon: 'assets/images/icons8-cycling-mountain-bike-50.png'},
@@ -60,61 +60,59 @@ function mapMarker(map) {
     
 
 // Yosamite     
-const climb = [{ name: "Google Location 1", lat: 37.865101, lng: -119.538330 },
-    { name: "Google Location 2", lat: 37.746036, lng: -119.53294 },
-    { name: "Google Location 3", lat: 38.8015, lng: -120.133 },
+const climb = [{ name: "The climb of your life", lat: 37.865101, lng: -119.538330 },
+    { name: "Don't climb this free solo style", lat: 37.746036, lng: -119.53294 },
+    { name: "Once you've reached the top, you'll see the world", lat: 38.8015, lng: -120.133 },
     { map: map1},
     { title: 'Yosamite has some epic climbs for you to scale. The greatest park in the world that you can visit'},
     {icon: 'assets/images/icons8-trekking-50.png'},
     ];
     
 // Everest    
-const climbe = [{ name: "Google Location 1", lat: 27.986065, lng: 86.922623 },
-    { name: "Google Location 2", lat: 28.004333316, lng: 86.855663244 },
-    { name: "Google Location 3", lat: 27.816667, lng: 86.716667 },
+const climbe = [{ name: "The most amazing territory", lat: 27.986065, lng: 86.922623 },
+    { name: "This is the greatest moment of your life", lat: 28.004333316, lng: 86.855663244 },
+    { name: "Reach the highest peak known to man", lat: 27.816667, lng: 86.716667 },
     { map: map2},
     { title: 'Mount Everest is the ultimate climbers experience with epic conditions to conquer and climb'},
     {icon: 'assets/images/icons8-trekking-50.png'},
     ];
     
 // Banzai  
-const surf = [{ name: "Google Location 1", lat: 21.664019, lng: -158.053852 },
-    { name: "Google Location 2", lat: 21.640296, lng: -158.063779 },
-    { name: "Google Location 3", lat: 21.659830694, lng: -158.053166454 },
+const surf = [{ name: "Surf the waves you dreamed of", lat: 21.664019, lng: -158.053852 },
+    { name: "Wax your board and feel the ride", lat: 21.640296, lng: -158.063779 },
+    { name: "The ultimate surf", lat: 21.659830694, lng: -158.053166454 },
     { map: map3},
     { title: 'Banzai has the greatest waves and so the greatest surf known to man. When that wave hits, make sure your ready'},
     {icon: 'assets/images/icons8-surfing-50.png'},
     ];
 
 // French Alps
-const ski = [{ name: "Google Location 1", lat: 45.45916483, lng: 6.90249639 },
-    { name: "Google Location 2", lat: 45.292165498, lng: 6.574664368 },
-    { name: "Google Location 3", lat: 45.004333316, lng: 6.119666188 },
+const ski = [{ name: "The most beautiful skiing location in the world", lat: 45.45916483, lng: 6.90249639 },
+    { name: "The fastest course we have in France", lat: 45.292165498, lng: 6.574664368 },
+    { name: "The sun, the snow and the beauty of France", lat: 45.004333316, lng: 6.119666188 },
     { map: map4},
     { title: 'The French alps give you the thrilling downhill skiing adventure of your life. Beautiful views and perfect snow to glide over'},
     {icon:  'assets/images/icons8-skiing-50.png'},
     ];
 
 // Victoria Falls    
-const jump = [{ name: "Google Location 1", lat: -17.924444, lng: 25.856667 },
-    { name: "Google Location 2", lat: 17.9318, lng: 25.8256 },
-    { name: "Google Location 3", lat: 17.9049, lng: 25.9379 },
+const jump = [{ name: "The jump into the blue", lat: -17.924444, lng: 25.856667 },
     {map: map5},
     { title: 'Victoria Falls skydive jump will blow your mind. You will feel like your flying high as you soar across the sky'},
     {icon: 'assets/images/icons8-skydiving-50.png'},
     ];
 
 // Netune Shark Dive    
-const dive = [{ name: "Google Location 1", lat: -35.336240, lng: 136.118428 },
+const dive = [{ name: "See the most dangerous creature in the sea", lat: -35.336240, lng: 136.118428 },
     {map: map6},
     { title: 'Neptune Islands Shark dives will face you with the oceans ultimate predator. When its fin breaches the surface, your heart wil stop'},
     {icon: 'assets/images/icons8-shark-50.png'},
     ];
 
 // Nile 
-const cruise = [{ name: "Google Location 1", lat: 29.533438, lng: 31.270695 },
-    { name: "Google Location 2", lat: 21.701324, lng: 31.176766 },
-    { name: "Google Location 3", lat: 16.783861, lng: 32.898682 },
+const cruise = [{ name: "Cruise the greatest river in the world", lat: 29.533438, lng: 31.270695 },
+    { name: "See the sights of Egypt", lat: 21.701324, lng: 31.176766 },
+    { name: "An amaainzg wondeful experience", lat: 16.783861, lng: 32.898682 },
     {map: map7},
     { title: 'The River Nile is one of the most beautiful and breathtaking places in the world. A truly wonderful place to visit'},
     {icon: 'assets/images/icons8-sail-boat-50.png'}
@@ -123,9 +121,9 @@ const cruise = [{ name: "Google Location 1", lat: 29.533438, lng: 31.270695 },
 // Dubai
 const buggy = [
     {
-    name: "Google Location 1", lat: 25.1840, lng: 55.2596 },
-    { name: "Google Location 2", lat: 25.1872, lng: 55.2812 },
-    { name: "Google Location 3", lat: 25.2768, lng: 55.3407 },
+    name: "See the sand fly as you jump the desert", lat: 25.1840, lng: 55.2596 },
+    { name: "This.. Is... The.. Greatest.. Experience", lat: 25.1872, lng: 55.2812 },
+    { name: "For all petrolheads ready to hit the ground hard.", lat: 25.2768, lng: 55.3407 },
     {map: map8},
     { title: 'Dune Buggying in Dubai is for serious petrol heads. The speed of racing around the desert is the mind blowing thrill'},
     ];
